b6b31be6446039c9ee2e7ae7022f4f200fb05fe0
Jakob Polegek <jakobpolegek@hotmail.com>
2025-01-19 08:31:02 +0000

    -updated text on welcome page.

--- src/page/WelcomePage.tsx
+++ src/page/WelcomePage.tsx
@@ -2,10 +2,14 @@ import { SignInButton } from "@clerk/clerk-react";
 
 const WelcomePage = () => {
   return (
-    <div className="flex flex-col items-center justify-center text-primary  bg-slate-900 h-screen">
-      <h1 className="text-3xl font-bold">Hi! You need to sign in.</h1>
-      <br />
-      <SignInButton />
+    <div className="flex flex-col items-center text-center justify-center text-primary  bg-slate-900 h-screen">
+      <p className="text-3xl font-bold mb-4">Hello!</p>
+      <p className="text-2xl font-bold">
+        Before using this app, you need to<br></br>&nbsp;
+        <SignInButton>
+          <button>sign in.</button>
+        </SignInButton>
+      </p>
     </div>
   );
 };
