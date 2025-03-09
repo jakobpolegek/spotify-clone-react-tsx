391f7afcae51f3dc277e8cea8bb091d01d00b7b1
Jakob Polegek <jakobpolegek@hotmail.com>
2025-03-09 11:05:59 +0100

    -started updaing playlist css, -started adding additional functionality on playlist page(play, context menu..)

--- src/page/PlaylistPage.tsx
+++ src/page/PlaylistPage.tsx
@@ -6,6 +6,7 @@ import {
   ContextMenuTrigger,
 } from "../components/ui/context-menu";
 import { Spinner } from "../components/ui/spinner";
+import { PlayCircleIcon, EllipsisIcon } from "lucide-react";
 import { useUser } from "@clerk/clerk-react";
 import usePlaylistSongs from "../hooks/usePlaylistSongs";
 import { useParams } from "react-router-dom";
@@ -25,21 +26,30 @@ const PlaylistPage = () => {
         <Spinner show={loading} size="large" />
       ) : (
         <div className="mr-5 col-span-9 row-span-11 bottom-0 bg-slate-800 rounded">
-          {playlistSongs.length > 0 ? (
-            <p className="text-8xl font-extrabold ml-10 mt-10 text-white">
-              {playlistSongs[0].name}
+          <div id="header" className="border-b-4 border-slate-900 pb-4">
+            {playlistSongs.length > 0 ? (
+              <p className="text-8xl font-extrabold ml-10 mt-10 text-white">
+                {playlistSongs[0].name}
+              </p>
+            ) : (
+              <p className="text-8xl font-extrabold ml-10 mt-10 text-white">
+                No songs in this playlist.
+              </p>
+            )}
+            <p className="ml-10 mt-6 text-gray-400">
+              {user.firstName}
+              {!loading && <>, {playlistSongs.length} songs</>}
             </p>
-          ) : (
-            <p className="text-8xl font-extrabold ml-10 mt-10 text-white">
-              No songs in this playlist.
-            </p>
-          )}
-          <p className="ml-10 mt-6 text-gray-400">
-            {user.firstName}
-            {!loading && <>, {playlistSongs.length} songs</>}
-          </p>
-
-          <div className="flex flex-col items-left justify-left max-h-screen grow">
+          </div>
+          <div id="playlistControls" className="flex flex-row ml-10">
+            <PlayCircleIcon className="mt-6 text-primary h-12 w-12"/>
+            <EllipsisIcon
+              className="mt-6 text-primary ml-4 h-12 w-12"
+              size={24}
+              onClick={() => fetchPlaylistSongs()}
+            />
+          </div>
+          <div className="flex flex-col items-left justify-left max-h-screen grow mt-12">
             <div id="songs" className="m-2 flex flex-col">
               {playlistSongs.map((song) => (
                 <ContextMenu key={song.source}>
